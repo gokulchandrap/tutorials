ac3c8609f01b911e09416c95dd789157d7a99a3a
Gokul Chandra <gokulpch@bu.edu>
2018-04-01 21:31:45 -0700

    Update dockerise.sh

--- mesos-marathon/dockerise.sh
+++ mesos-marathon/dockerise.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -e
-docker login -u gokulpch -p USHArani568
+docker login -u gokulpch -p $DOCKER_PASSWORD
 docker build -t gokulpch/mesos-marathon-demo:$BUILD_NUMBER .
 docker push gokulpch/mesos-marathon-demo:$BUILD_NUMBER
